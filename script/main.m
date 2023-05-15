45c90f7b9422286bf08b1b1107aaa0d8b96333f8
WelcomeToWonderland <xianghui.que@foxmail.com>
2023-05-15 14:07:11 +0800

    日常更新

--- script/main.m
+++ script/main.m
@@ -22,36 +22,36 @@ temp2 = 'D:\workspace\dataset\OABreast\dat2mat';
 % suffix_path = 'HR';
 % mat_field = 'img';
 
-% % translated
-% is_untranslated = false;
-% is_unclipping = true;
-% prefix_path = 'D:\workspace\dataset\OABreast\dat2mat\clipping\pixel_translation';
-% suffix_path = 'LR\X2';
-% mat_field = 'img';
-% 
-% reset = true;
-% basenames = {'Neg_07_Left', 'Neg_35_Left', 'Neg_47_Left', 'Neg_07_Left_train', 'Neg_07_Left_test'};
-% for idx = 1:5
-%     basename = basenames(idx);
-%     basename = char(basename);
-% %     folder_save = basename;
-%     folder_save = strcat(basename, '_LR');
-% %     path = fullfile(prefix_path, basename, suffix_path);
-%     filename = strcat(basename, '.mat');
-%     path = fullfile(prefix_path, basename, suffix_path, filename);
-%     show_3d(path, folder_save, is_untranslated, is_unclipping, basename, reset, mat_field);
-% end
-
-% µ¥²â
-mat_field = 'img';
-path = 'D:\workspace\dataset\oabreast_2d_correct_new_start\results-Neg_07_Left_test\Neg_07_Left_test_x2_SR.mat';
-folder_save = 'Neg_07_Left_test_HAN_new_start_SR';
+% translated
 is_untranslated = false;
 is_unclipping = true;
-basename = 'Neg_07_Left_test';
+prefix_path = 'D:\workspace\dataset\OABreast\dat2mat\clipping\pixel_translation';
+suffix_path = 'SR\X2';
+mat_field = 'img';
+
 reset = true;
-mat_field = 'f1';
-show_3d(path, folder_save, is_untranslated, is_unclipping, basename, reset, mat_field);
+basenames = {'Neg_07_Left', 'Neg_35_Left', 'Neg_47_Left', 'Neg_07_Left_train', 'Neg_07_Left_test'};
+for idx = 1:5
+    basename = basenames(idx);
+    basename = char(basename);
+%     folder_save = basename;
+    folder_save = strcat(basename, '_SR');
+%     path = fullfile(prefix_path, basename, suffix_path);
+    filename = strcat(basename, '.mat');
+    path = fullfile(prefix_path, basename, suffix_path, filename);
+    show_3d(path, folder_save, is_untranslated, is_unclipping, basename, reset, mat_field);
+end
+
+% µ¥²â
+% mat_field = 'img';
+% path = 'D:\workspace\dataset\oabreast_2d_correct_new_start\results-Neg_07_Left_test\Neg_07_Left_test_x2_SR.mat';
+% folder_save = 'Neg_07_Left_test_HAN_new_start_SR';
+% is_untranslated = false;
+% is_unclipping = true;
+% basename = 'Neg_07_Left_test';
+% reset = true;
+% mat_field = 'f1';
+% show_3d(path, folder_save, is_untranslated, is_unclipping, basename, reset, mat_field);
 
 
 
